863a20688a079861fca3b27a555572b0dc51f3f9
Akash Ramani <akashr@codemechsolutions.com>
2026-01-10 14:16:35 +0530

    Menu changes done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,17 @@
 'use client';
-import Landings from '@/assets/img/landings.jpg';
 import Logo from '@/assets/img/mcwinItechLogoLight.png';
-import Image from 'next/image';
+import Image, {StaticImageData} from 'next/image';
 import Link from 'next/link';
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Collapse, Offcanvas, OffcanvasBody, OffcanvasHeader} from 'react-bootstrap';
 import IconifyIcon from '../IconifyIcon';
 import ThemeToggle from '../ThemeToggle';
 import {usePathname} from 'next/navigation';
+
+import brandIcon from '@/assets/img/services/brand.png';
+import growIcon from '@/assets/img/services/grow.png';
+import buildIcon from '@/assets/img/services/build.png';
+import scaleIcon from '@/assets/img/services/scale.png';
 
 type DropdownItem = {
     label: string;
@@ -22,6 +26,7 @@ type DropdownLink = {
 
 type DropdownSection = {
     title: string;
+    icon: StaticImageData;
     links: DropdownLink[];
 };
 
@@ -69,99 +74,54 @@ const Navbar = ({
         setOpenMobileDropdown(openMobileDropdown === menu ? null : menu);
     };
 
-    const landingsMenu: DropdownColumn1[] = [
-        [
-            {label: 'Template Intro Page', href: '/home'},
-            {label: 'Mobile App Showcase v.1', href: '/app-showcase-v1'},
-            {label: 'Mobile App Showcase v.2', href: '/app-showcase-v2'},
-            {label: 'Mobile App Showcase v.3', href: '/app-showcase-v3', badge: 'New'},
-            {label: 'Product Landing', href: '/product'},
-            {label: 'SaaS v.1', href: '/saas-v1'},
-            {label: 'SaaS v.2', href: '/saas-v2'},
-            {label: 'SaaS v.3', href: '/saas-v3'},
-            {label: 'SaaS v.4', href: '/saas-v4'},
-            {label: 'SaaS v.5', href: '/saas-v5', badge: 'New'},
-        ],
-        [
-            {label: 'Startup', href: '/startup'},
-            {label: 'Financial Consulting', href: '/financial'},
-            {label: 'Online Courses', href: '/online-courses'},
-            {label: 'Medical', href: '/medical'},
-            {label: 'Software Dev Agency v.1', href: '/software-dev-agency-v1'},
-            {label: 'Software Dev Agency v.2', href: '/software-dev-agency-v2'},
-            {label: 'Software Dev Agency v.3', href: '/software-dev-agency-v3'},
-            {label: 'Conference', href: '/conference'},
-            {label: 'Digital Agency', href: '/digital-agency'},
-            {label: 'Blog Homepage', href: '/blog'},
-        ],
-    ];
-
     const pagesMenu: DropdownColumn2[] = [
         [
             {
-                title: 'About',
+                title: 'Brand',
+                icon: brandIcon,
                 links: [
-                    {label: 'About v.1', href: '/about-v1'},
-                    {label: 'About v.2', href: '/about-v2'},
-                    {label: 'About v.3', href: '/about-v3'},
-                ],
-            },
-            {
-                title: 'Blog',
-                links: [
-                    {label: 'List View with Sidebar', href: '/blog-list-with-sidebar'},
-                    {label: 'Grid View with Sidebar', href: '/blog-grid-with-sidebar'},
-                    {label: 'List View no Sidebar', href: '/blog-list-no-sidebar'},
-                    {label: 'Grid View no Sidebar', href: '/blog-grid-no-sidebar'},
-                    {label: 'Simple Feed', href: '/blog-simple-feed'},
-                    {label: 'Single Post', href: '/blog-single'},
-                    {label: 'Podcast', href: '/blog-podcast'},
+                    {label: 'Branding', href: '/brand'},
+                    {label: 'Web Design', href: '/brand'},
+                    {label: 'UI/UX Design', href: '/brand'},
+                    {label: 'Mobile App Design', href: '/brand'},
                 ],
             },
         ],
         [
             {
-                title: 'Portfolio',
+                title: 'Build',
+                icon: buildIcon,
                 links: [
-                    {label: 'Grid View', href: '/portfolio-grid'},
-                    {label: 'List View', href: '/portfolio-list'},
-                    {label: 'Slider View', href: '/portfolio-slider'},
-                    {label: 'Courses', href: '/portfolio-courses'},
-                    {label: 'Single Project', href: '/portfolio-single-project'},
-                    {label: 'Single Course', href: '/portfolio-single-course'},
-                ],
-            },
-            {
-                title: 'Services',
-                links: [
-                    {label: 'Services v.1', href: '/services-v1'},
-                    {label: 'Services v.2', href: '/services-v2'},
-                    {label: 'Service Details v.1', href: '/services-single-v1'},
-                    {label: 'Service Details v.2', href: '/services-single-v2'},
+                    {label: 'Websites', href: '/build'},
+                    {label: 'eCommerce', href: '/build'},
+                    {label: 'Mobile Apps', href: '/build'},
+                    {label: 'Custom Software', href: '/build'},
                 ],
             },
         ],
         [
             {
-                title: 'Pricing',
-                links: [{label: 'Pricing Page', href: '/pricing'}],
-            },
-            {
-                title: 'Contacts',
+                title: 'Grow',
+                icon: growIcon,
                 links: [
-                    {label: 'Contacts v.1', href: '/contacts-v1'},
-                    // { label: 'Contacts v.2', href: '/contacts-v2' },
-                    {label: 'Contacts v.3', href: '/contacts-v3'},
+                    {label: 'Social Media Marketing', href: '/grow'},
+                    {label: 'Search Engine Optimisation', href: '/grow'},
+                    {label: 'Search Engine Marketing', href: '/grow'},
+                    {label: 'Pay-Per-Click', href: '/grow'},
                 ],
             },
+        ],
+        [
             {
-                title: 'Specialty',
+                title: 'Scale',
+                icon: scaleIcon,
                 links: [
-                    {label: '404 Error v.1', href: '/404-v1'},
-                    {label: '404 Error v.2', href: '/404-v2'},
+                    {label: 'Growth Accelerator', href: '/scale'},
+                    {label: 'Business Scaling Solutions', href: '/scale'},
+                    {label: 'Revenue Growth Engine', href: '/scale'},
+                    {label: 'Market Expansion Services', href: '/scale'},
                 ],
             },
-
         ],
     ];
 
@@ -172,14 +132,11 @@ const Navbar = ({
         {label: 'Scale', href: '/scale'},
     ];
 
-    const allLandingsLinks = landingsMenu.flat();
     const allPagesLinks = pagesMenu.flat().flatMap(section => section.links);
     const allAccountLinks = accountMenu;
 
 
-    const isLandingsActive = isParentActive(allLandingsLinks, pathname);
     const isPagesActive = isParentActive(allPagesLinks, pathname);
-    const isAccountActive = isParentActive(allAccountLinks, pathname);
 
     return (
         <header className={`${Headerclass} ${isSticky && headerSticky ? headerSticky : ''}`}>
@@ -204,73 +161,20 @@ const Navbar = ({
                     </OffcanvasHeader>
                     <OffcanvasBody className="offcanvas-body">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className={`nav-item dropdown ${isLandingsActive ? 'active' : ''}`}>
-                                <button
-                                    type="button"
-                                    className="nav-link"
-                                    onClick={() => toggleMobileDropdown('landings')}
-                                    aria-expanded={openMobileDropdown === 'landings'}
-                                >
-                  <span className="d-flex gap-1 align-items-center">
-                    Landings
-                    <IconifyIcon icon="bx:chevron-down" fontSize={18}/>
-                  </span>
-                                </button>
-                                <Collapse in={openMobileDropdown === 'landings'}>
-                                    <div className="dropdown-menu p-0">
-                                        <div className="d-lg-flex">
-                                            <div
-                                                className="mega-dropdown-column d-flex justify-content-center align-items-center rounded-3 rounded-end-0 px-0"
-                                                style={{margin: '-1px', backgroundColor: '#f3f6ff'}}
-                                            >
-                                                <Image src={Landings} alt="Landings"/>
-                                            </div>
-                                            {landingsMenu.map((column, colIndex) => (
-                                                <div
-                                                    key={colIndex}
-                                                    className="mega-dropdown-column pt-lg-3 pb-lg-4"
-                                                    style={
-                                                        colIndex === 0
-                                                            ? {['--si-mega-dropdown-column-width' as string]: '15rem'}
-                                                            : {}
-                                                    }
-                                                >
-                                                    <ul className="list-unstyled mb-0">
-                                                        {column.map((item, idx) => (
-                                                            <li key={idx}>
-                                                                <Link
-                                                                    href={item.href}
-                                                                    className={`dropdown-item d-flex align-items-center ${pathname === item.href ? 'active' : ''
-                                                                    }`}
-                                                                    onClick={() => setShowMenu(false)}
-                                                                >
-                                                                    {item.label}
-                                                                    {item.badge && (
-                                                                        <span
-                                                                            className="badge bg-success ms-2">{item.badge}</span>
-                                                                    )}
-                                                                </Link>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </div>
-                                </Collapse>
+                            <li className={`nav-item dropdown ${pathname === '/home' ? 'active' : ""}`}>
+                                <Link href="/about" className="nav-link">Home</Link>
                             </li>
-
                             <li className={`nav-item dropdown ${isPagesActive ? 'active' : ''}`}>
                                 <button
                                     type="button"
                                     className="nav-link"
-                                    onClick={() => toggleMobileDropdown('pages')}
-                                    aria-expanded={openMobileDropdown === 'pages'}
+                                    onClick={() => toggleMobileDropdown('Services')}
+                                    aria-expanded={openMobileDropdown === 'Services'}
                                 >
-                  <span className="d-flex gap-1 align-items-center">
-                    Pages
-                    <IconifyIcon icon="bx:chevron-down" fontSize={18}/>
-                  </span>
+                                  <span className="d-flex gap-1 align-items-center">
+                                    Services
+                                    <IconifyIcon icon="bx:chevron-down" fontSize={18}/>
+                                  </span>
                                 </button>
                                 <Collapse in={openMobileDropdown === 'pages'}>
                                     <div className="dropdown-menu">
@@ -279,7 +183,17 @@ const Navbar = ({
                                                 <div key={colIndex} className="mega-dropdown-column">
                                                     {column.map((section, secIndex) => (
                                                         <div key={secIndex}>
-                                                            <h6 className="px-3 mb-2">{section.title}</h6>
+                                                            <div className="d-flex align-items-center">
+                                                                <Image
+                                                                    style={{ paddingRight: "8px", maxWidth: "100px", marginTop: "-2px" }}
+                                                                    src={section.icon}
+                                                                    alt={section.title}
+                                                                    width={35}
+                                                                    height={35}
+                                                                    priority
+                                                                />
+                                                                <h4 className="mb-2">{section.title}</h4>
+                                                            </div>
                                                             <ul className="list-unstyled mb-3">
                                                                 {section.links.map((link, linkIndex) => (
                                                                     <li key={linkIndex}>
@@ -303,51 +217,22 @@ const Navbar = ({
                                 </Collapse>
                             </li>
 
-                            <li className={`nav-item dropdown ${isAccountActive ? 'active' : ''}`}>
-                                <button
-                                    type="button"
-                                    className="nav-link"
-                                    onClick={() => toggleMobileDropdown('account')}
-                                    aria-expanded={openMobileDropdown === 'account'}
-                                >
-                  <span className="d-flex gap-1 align-items-center">
-                    Services
-                    <IconifyIcon icon="bx:chevron-down" fontSize={18}/>
-                  </span>
-                                </button>
-                                <Collapse in={openMobileDropdown === 'account'}>
-                                    <ul className="dropdown-menu">
-                                        {accountMenu.map((item, index) => (
-                                            <li key={index}>
-                                                <Link
-                                                    href={item.href}
-                                                    className={`dropdown-item ${pathname === item.href ? 'active' : ''}`}
-                                                    onClick={() => setShowMenu(false)}
-                                                >
-                                                    {item.label}
-                                                </Link>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                </Collapse>
-                            </li>
                             <li className={`nav-item dropdown ${pathname === '/about' ? 'active' : ""}`}>
                                 <Link href="/about" className="nav-link">About Us</Link>
                             </li>
                             <li className={`nav-item dropdown ${pathname === '/contact' ? 'active' : ""}`}>
-                                <Link href="/contact" className="nav-link">Contact</Link>
+                                <Link href="/contact" className="nav-link">Contact Us</Link>
                             </li>
                         </ul>
                     </OffcanvasBody>
                     <div className="offcanvas-header border-top">
                         <a
-                            href="#"
+                            href="/contact"
                             className="btn btn-primary w-100"
                             rel="noopener"
-                            onClick={() => setShowMenu(false)}
                         >
                             <IconifyIcon icon="bx:cart" className="fs-4 lh-1 me-1"/>
-                            &nbsp;Buy now
+                            &nbsp;Contact Now
                         </a>
                     </div>
                 </Offcanvas>
@@ -363,12 +248,12 @@ const Navbar = ({
                 </button>
                 <Button
                     as="a"
-                    href="#"
+                    href="/contact"
                     className="btn btn-primary btn-sm fs-sm rounded d-none d-lg-inline-flex"
                     rel="noopener"
                 >
-                    <IconifyIcon icon="bx:cart" className="fs-5 lh-1 me-1"/>
-                    &nbsp;Buy now
+                    <IconifyIcon icon="bx:phone" className="fs-5 lh-1 me-1"/>
+                    &nbsp;Contact Now
                 </Button>
             </div>
         </header>
