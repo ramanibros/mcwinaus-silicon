a1e5ed69acaaee135518f5537711c4556e2eb421
Kunikagera <138278077+Kunikagera@users.noreply.github.com>
2026-02-14 13:28:24 +0530

    blog-copy

---
+++ src/app/(landings)/blog/custom-website-vs-template-right-for-business copy/page.tsx
@@ -0,0 +1,67 @@
+import IconifyIcon from '@/components/IconifyIcon';
+import Link from 'next/link';
+import PostContent from './components/PostContent';
+import PostTitle from './components/PostTitle';
+import NavbarPage from "@/components/navbar/Navbar-page";
+import Subscribe from "@/components/common/subscribe";
+import Footer from "@/components/common/Footer";
+
+export const metadata = {
+    metadataBase: new URL("https://www.mcwinitech.com.au/"),
+
+    title: "Accelerate business growth in Perth | Expert IT Development & Digital Marketing Services",
+    description:
+        "Drive your business to success in Perth with our expert IT development and digital marketing services. We create customized strategies to optimize your online presence, engage your target audience, and drive significant and sustainable business growth. Partner with us today to unleash your full potential and achieve remarkable success in Perth.",
+    keywords: [
+        "Software Development Services",
+        "Custom Software Development",
+        "Web Application Development",
+        "Mobile App Development",
+        "Full-Stack Development",
+    ],
+
+    alternates: {
+        canonical: "https://www.mcwinitech.com.au/blog/custom-website-vs-template-right-for-business/",
+    }
+};
+
+const Page = () => {
+    return (
+        <>
+            <NavbarPage
+                Headerclass="header navbar navbar-expand-lg bg-light navbar-sticky"
+                headerSticky="navbar-stuck"
+            />
+            <nav className="container pt-4 mt-lg-3" aria-label="breadcrumb">
+                <nav className="container pt-4 mt-lg-3" aria-label="breadcrumb">
+                    <ol className="breadcrumb mb-0">
+                        <li className="breadcrumb-item">
+                            <Link href="/">
+                                <IconifyIcon icon="bx:home-alt" className="fs-lg me-1"/>
+                                Home
+                            </Link>
+                        </li>
+                        <span className="d-flex align-items-center mx-2">
+              <IconifyIcon icon="bx:chevrons-right"/>
+            </span>
+                        <li className="breadcrumb-item">
+                            <Link href="/blog">Blog</Link>
+                        </li>
+                        <span className="d-flex align-items-center mx-2">
+              <IconifyIcon icon="bx:chevrons-right"/>
+            </span>
+                        <li className="breadcrumb-item active" aria-current="page">
+                            Custom Website vs. Template: Which Is Right for Your Business?
+                        </li>
+                    </ol>
+                </nav>
+            </nav>
+            <PostTitle/>
+            <PostContent/>
+            <Subscribe/>
+            <Footer/>
+        </>
+    );
+};
+
+export default Page;
